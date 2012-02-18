86a46f973ea18f395a84adcad36172c1ea90a1ed
Dustin Hoffman <dustin.hoffman@breefield.com>
2012-02-17 23:24:04 -0800

    Add cap bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,8 @@ role :web, "173.230.146.14"                          # Your HTTP server, Apache/
 role :app, "173.230.146.14"                          # This may be the same as your `Web` server
 role :db,  "173.230.146.14", :primary => true # This is where Rails migrations will run
 
+require "bundler/capistrano"
+
 set :user, "rose"
 set :use_sudo, false
 
